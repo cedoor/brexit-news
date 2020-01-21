95e2c196b497b8c19663e4d6e7c876b80f8dea1c
cedoor <omardesogus9@gmail.com>
2020-01-21 11:54:15 +0100

    [feat] add new newspaper entries

--- src/start.py
+++ src/start.py
@@ -1,5 +1,8 @@
+import src.daily_mirror as daily_mirror
+import src.daily_star as daily_star
 import src.the_guardian as the_guardian
 import src.the_sun as the_sun
+import src.the_telegraph as the_telegraph
 
 
 def start(newspaper):
@@ -7,13 +10,19 @@ def start(newspaper):
 
     return {
         "0": the_guardian.start,
-        "1": the_sun.start
+        "1": the_sun.start,
+        "2": daily_star.start,
+        "3": the_telegraph.start,
+        "4": daily_mirror.start
     }[newspaper]()
 
 
 print("""
 [0] The Guardian
 [1] The Sun
+[2] Daily Star
+[3] The Telegraph
+[4] Daily Mirror
 
 Choose the newspaper to scrape: """, end="")
 
