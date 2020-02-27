37ea5abaa59da00b07b2ffacbe837e33dd2c8e3a
Enrico <e.ipodda@gmail.com>
2020-02-27 15:22:27 +0100

    [feat] add error handler
    when an error is catch, delay for some seconds and retry. connection errors are more frequent

--- src/start.py
+++ src/start.py
@@ -1,9 +1,15 @@
+import sys
+import time
+
 import src.daily_mirror as daily_mirror
 import src.daily_star as daily_star
 import src.indipendent as indipendent
 import src.the_guardian as the_guardian
 import src.the_sun as the_sun
 import src.the_telegraph as the_telegraph
+
+
+max_attempts = 3
 
 
 def run_all():
@@ -13,6 +19,48 @@ def run_all():
     the_telegraph.start()
     daily_mirror.start()
     indipendent.start()
+
+
+def get_error_info(error):
+    for info in error:
+        print("\t%s" % info)
+
+
+def error_handler(newspaper):
+    for attempt in range(max_attempts + 1):
+        try:
+            {
+                "0": daily_mirror.start,
+                "1": daily_star.start,
+                "2": indipendent.start,
+                "3": the_guardian.start,
+                "4": the_sun.start,
+                "5": the_telegraph.start,
+                "9": run_all
+            }[newspaper]()
+            break
+
+        except: # catch *all* exceptions
+            error = sys.exc_info()
+
+            if attempt >= max_attempts:
+                error = sys.exc_info()
+                print("\n\nToo many attempts. Closing program")
+                print("Last error:")
+                get_error_info(error)
+                break
+            
+            print("\n\nAn error occurred:")
+            get_error_info(error)
+            print()
+
+            seconds = 10 * (attempt + 1)
+            while seconds >= 0:
+                sys.stdout.write("\rRestarting function in %s sec\033[K" % (seconds))
+                time.sleep(1)
+                seconds -= 1
+            
+            print("\n\n")
 
 
 newspaper = input("""
@@ -29,13 +77,4 @@ newspaper = input("""
 Choose the newspaper to scrape: """)
 
 print()
-
-{
-    "0": daily_mirror.start,
-    "1": daily_star.start,
-    "2": indipendent.start,
-    "3": the_guardian.start,
-    "4": the_sun.start,
-    "5": the_telegraph.start,
-    "9": run_all
-}[newspaper]()
+error_handler(newspaper)
